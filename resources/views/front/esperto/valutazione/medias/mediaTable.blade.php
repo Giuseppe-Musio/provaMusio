bcfacaee769f4f112cf0e472cec868886ed32e4b
Giuseppe-Musio <40686760+Giuseppe-Musio@users.noreply.github.com>
2018-07-02 16:49:31 +0200

    Risoluzione bug video

--- resources/views/front/esperto/valutazione/medias/mediaTable.blade.php
+++ resources/views/front/esperto/valutazione/medias/mediaTable.blade.php
@@ -1,4 +1,17 @@
 @if(sizeof($medias) > 0)
+<style> 
+.mediaaaa
+{
+visibility: hidden;
+position: absolute;
+top: 50%;
+left: 50%;
+transform: translate(-50%,-50%);
+}
+
+
+</style>
+</style>
     <div class="row" style="margin-top: 30px;">
         <div class="col-md-12">
             {{ $medias->appends(request()->input())->links() }}
@@ -14,7 +27,7 @@
                                     <th>Email</th>
                                     <th>Obiettivo Task</th>
                                     <th>Data Completamento</th>
-                                    <th></th>
+                                    <th>Video</th>
                                 </tr>
                                 </thead>
                                 @endif
@@ -24,27 +37,61 @@
                                     <td>{{$media->task->goal}}</td>
                                     <td>$media->studyUser->studyUserTask->first()->created_at->toDateTimeString()}}</td>
 
-                                    @if($media->type == 'video/webm')
-                                        <td>
-                                            <a id="buttonMedia{{$i}}" class="btn btn-primary" name="{{ $media->path }}"
-                                               onclick="showMedia(this, '{{ URL::asset('assets/js/plugin/video/video-js.js') }}', '{{ $media->type }}' )">
-                                                Riproduci {{ $button_name }}
-                                            </a>
-                                        </td>
+                                    <td>
+                                        <button class="btn btn-primary" onclick="playVid({{$i}})" type="button">Riproduci Video</button>
+
+                                    </td>
+
+                                    
+                                @if($media->type == 'video/webm')
+                                
+                                        
+                                     <video class='mediaaaa' id='{{$i}}' width = "620" height = "400" controls preload="none" > 
+
+                                       
+                                               <source src = "{{asset("storage/video/$media->file_name")}}" 
+                                                type="video/webm">
+  
+                                            
+                                        </video>
+                                        
                                     @elseif($media->type == 'audio/ogg')
-                                        <td>
-                                            <a id="buttonMedia{{$i}}" class="btn btn-primary" name="{{ $media->path }}"
-                                               onclick="showMedia(this, '{{ URL::asset('assets/js/plugin/audio/audio.js') }}', '{{ $media->type }}' )">
-                                                {{ $button_name }}
-                                            </a>
-                                        </td>
-                                    @endif
+                                        
+                                          
+                                     <video class='mediaaaa' id='{{$i}}' width="620" height = "400" controls preload="none" > 
+
+                                       
+                                               <source src = "{{asset("storage/audio/$media->file_name")}}" 
+                                                type="video/webm">
+  
+                                            
+                                        </video>
+                                        
+                                    @endif 
+                                   
                                 </tr>
+
+                                <script>
+                                        function playVid(num) { 
+                                        var vid=document.getElementById(num);
+                                        vid.style.visibility="visible";
+
+                                         if (vid.paused){ 
+                                            vid.play();
+                                        }
+                                              vid.onended = function() {
+                                                vid.style.visibility="hidden";
+                                          };
+                                        
+                                    } 
+                                </script> 
+
                                 @php $i++; @endphp
                                 @empty
-
                                 @endforelse
+
                             </table>
+
                 </div>
             </div>
             {{ $medias->appends(request()->input())->links() }}
